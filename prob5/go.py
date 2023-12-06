2ad7d286457eeb6f6468720df152b8d933946fcb
Zach Hindes <zach@dowhatnow.net>
2023-12-05 22:39:31 -0600

    prob 4: brute force works, but my brain hurts for a better algorithm. i can visualize it but writing it in code hurts my head :)

---
+++ prob5/go.py
@@ -0,0 +1,205 @@
+import logging
+import re
+import os
+from enum import IntEnum
+
+_logger = logging.getLogger(__name__)
+
+class MapType(IntEnum):
+    SEED_TO_SOIL = 0
+    SOIL_TO_FERTILIZER = 1
+    FERTILIZER_TO_WATER = 2
+    WATER_TO_LIGHT = 3
+    LIGHT_TO_TEMPERATURE = 4
+    TEMPERATURE_TO_HUMIDITY = 5
+    HUMIDITY_TO_LOCATION = 6
+
+MAP_LIST = [
+    (MapType.SEED_TO_SOIL, 'seed-to-soil'),
+    (MapType.SOIL_TO_FERTILIZER, 'soil-to-fertilizer'),
+    (MapType.FERTILIZER_TO_WATER, 'fertilizer-to-water'),
+    (MapType.WATER_TO_LIGHT, 'water-to-light'),
+    (MapType.LIGHT_TO_TEMPERATURE, 'light-to-temperature'),
+    (MapType.TEMPERATURE_TO_HUMIDITY, 'temperature-to-humidity'),
+    (MapType.HUMIDITY_TO_LOCATION, 'humidity-to-location'),
+]
+
+class MapInfo:
+    def __init__(self, dest_start, src_start, len):
+        self.dest_start = dest_start
+        self.src_start = src_start
+        self.len = len
+
+    def __repr__(self):
+        return f"{self.dest_start} {self.src_start} {self.len}"
+
+class Almanac:
+    def __init__(self):
+        self.seeds = []
+        self.maps = {
+            MapType.SEED_TO_SOIL: [],
+            MapType.SOIL_TO_FERTILIZER: [],
+            MapType.FERTILIZER_TO_WATER: [],
+            MapType.WATER_TO_LIGHT: [],
+            MapType.LIGHT_TO_TEMPERATURE: [],
+            MapType.TEMPERATURE_TO_HUMIDITY: [],
+            MapType.HUMIDITY_TO_LOCATION: [],
+        }
+
+    def add_seeds(self, seeds):
+        i = 0
+        while i < len(seeds):
+            start = seeds[i]
+            end = start + seeds[i+1]
+            self.seeds.append((start, end))
+            i += 2
+
+    def update_map(self, type, map_info):
+        self.maps[type].append(map_info)
+
+    def _map_src_to_dest(self, type, src):
+        map_list = self.maps[type]
+        for map in map_list:
+            if src >= map.src_start:
+                if src < map.src_start + map.len:
+                    offset = src - map.src_start
+                    return map.dest_start + offset
+                break
+        return src
+
+    def get_location(self, seed):
+        src = seed
+        for (type,_) in MAP_LIST:
+            dest = self._map_src_to_dest(type, src)
+            _logger.debug(f"{type} src {src} --> dest {dest}")
+            src = dest
+        _logger.info(f"seed {seed} --> location {dest}")
+        return dest
+
+    def get_lowest_location_brute(self):
+        lowest_location = -1
+        for (start, end) in self.seeds:
+            for seed in range(start, end):
+                location = self.get_location(seed)
+                lowest_location = min(location, lowest_location) if lowest_location > 0 else location
+        return lowest_location
+
+    def _get_srcs_for_dests(self, next_type, input):
+        _logger.debug(f"finding dests in {next_type} for dests {input}")
+        # input is next_type's dest
+        input_len = input.stop - input.start
+        for next_range in self.maps[next_type]:
+            if input.stop > next_range.dest_start + next_range.len:
+                continue
+            if input.start >= next_range.dest_start:
+                offset = input.start - next_range.dest_start
+                len_for_range = min(input_len, next_range.len)
+                input_len -= len_for_range
+                yield range(next_range.src_start + offset, len_for_range)
+            # we're done
+            if input_len == 0:
+                return
+
+        # and the last bit
+        yield range(next_range.src_start, next_range.src_start + input_len)
+
+    def _lowest_seed_in_range(self, seed_ranges):
+        pass
+
+    def prepare(self):
+        # sort the way we want
+        for type, map in self.maps.items():
+            map.sort(key=lambda map_info: map_info.dest_start)
+
+        # add a "fake" dest range in the last map
+        lowest_location_range = self.maps[MapType.HUMIDITY_TO_LOCATION][0]
+        new_lowest_location_range = MapInfo(0, 0, lowest_location_range.dest_start)
+        self.maps[MapType.HUMIDITY_TO_LOCATION].insert(0, new_lowest_location_range)
+
+    def get_lowest_location_fast(self):
+        for map_info in self.maps[MapType.HUMIDITY_TO_LOCATION]:
+            _logger.debug(f"lowest_location_info: {map_info}")
+            lowest_range = range(map_info.src_start, map_info.len)
+            next_ranges = list(self._get_srcs_for_dests(MapType.HUMIDITY_TO_LOCATION-1, lowest_range))
+            _logger.debug(f"next ranges: {next_ranges}")
+            # lowest_seed_in_ranges = self._lowest_seed_in_range(seed_ranges)
+            # if lowest_seed_in_ranges is not None:
+                # return lowest_seed_in_ranges
+
+
+    def __repr__(self):
+        result = f"seeds: {self.seeds}\n"
+        for type, map in self.maps.items():
+            result += (f"{type} map:\n")
+            result += (f"{str(map)}\n")
+        return result
+
+
+def get_almanac(input):
+    almanac = Almanac()
+
+    with open(input) as f:
+        lines = list(f)
+        line_idx = 0
+
+        # seeds: 79 14 55 13
+        seeds_match = re.match(r'seeds: ([\d ]+)', lines[line_idx])
+        if not seeds_match:
+            raise RuntimeError(f"failed to parse line {line_idx}: {lines[line_idx]}")
+        line_idx += 1
+        almanac.add_seeds([int(seed) for seed in seeds_match.group(1).split()])
+
+        # blank line
+        line_idx += 1
+        for (type, label) in MAP_LIST:
+            # seed-to-soil map:
+            map_label_match = re.match(fr'{label} map:', lines[line_idx])
+            if not map_label_match:
+                raise RuntimeError(f"failed to parse line {line_idx}: {lines[line_idx]}")
+            line_idx += 1
+
+            while line_idx < len(lines):
+                # 50 98 2
+                map_match = re.match(r'(\d+) (\d+) (\d+)', lines[line_idx])
+                line_idx += 1
+                # got a blank line
+                if not map_match:
+                    break
+
+                map_info = MapInfo(
+                    int(map_match.group(1)),
+                    int(map_match.group(2)),
+                    int(map_match.group(3)),
+                )
+                almanac.update_map(type, map_info)
+
+    almanac.prepare()
+    _logger.debug(f"Almanac: {str(almanac)}")
+    return almanac
+
+def main():
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input', required=True)
+    parser.add_argument('--verbose', '-v', action='count', default=0)
+    args = parser.parse_args()
+
+    verbosity = 2 - args.verbose
+    logging_level = {
+        0: logging.DEBUG,
+        1: logging.INFO,
+        2: logging.WARNING,
+        3: logging.ERROR,
+        4: logging.CRITICAL,
+    }[verbosity]
+    log_format = "[%(relativeCreated)6d] %(levelname)-5s %(funcName)s: %(message)s"
+    logging.basicConfig(level=logging_level, format=log_format)
+
+    almanac = get_almanac(args.input)
+    # print(f"Minimum seed location (brute): {almanac.get_lowest_location_brute()}")
+    print(f"Minimum seed location (fast): {almanac.get_lowest_location_fast()}")
+
+
+if __name__ == "__main__":
+    main()
